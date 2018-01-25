ed502266791d754123d174b461d0ebe9b1997969
Andressa Sivolella <andressasivolella@gmail.com>
2018-01-25 14:20:21 -0200

    Enable CORS on server side, so API can be used inside other domains.

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const express = require('express');
 const routes = require('./routes')
 const api = express();
 
+// Enable CORS
+api.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 api.use('/', routes);
 
 api.listen(
